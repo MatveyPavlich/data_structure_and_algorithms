81dfa325e1cde63b0356233f619632ac75486d33
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-02 22:57:45 +0100

    fix: fix the program

--- stack/stack_2.c
+++ stack/stack_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 struct node {
 	int value;
@@ -19,24 +20,24 @@ struct stack init(){
 void push(struct stack *s, int pushed_value){
 	
 	// struct node new_node = {pushed_value, NULL};
-	struct node new_node = malloc(sizeof(struct node)); // will be accessed at runtime => need malloc()
+	struct node *new_node = malloc(sizeof(struct node)); // will be accessed at runtime => need malloc()
 	new_node->value = pushed_value;	
 	// if(s->size != 0){
 	// 	s->top_node->next_node = new_node;
 	// } 
-	new_node->next_node = s->top;
+	new_node->next_node = s->top_node;
 	// s->top_node = &new_node;
-	s->top = new_node;
+	s->top_node = new_node;
 	s->size++;
 	printf("Push: %d\n", pushed_value);
 }
 
 
 int main(){
-	def my_stack = init();
+	struct stack my_stack = init();
 	push(&my_stack, 3);
 	push(&my_stack, 8);
 	push(&my_stack, 9);
 	
-	return 1;
+	return 0;
 }
