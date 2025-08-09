94d24445a7df166e77ac2dc892871210cef9da45
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-09 17:11:39 +0100

    feat: implement delete() into linked list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -45,16 +45,16 @@ Node* getLastNode(const LinkedList *l){
 }
 
 
-void search(const LinkedList *l, int val){
+Node* search(const LinkedList *l, int val){
     Node *current_node = l->first;
     while(1){
         if(current_node == NULL){ // Should be a first condition to not dereference a null pointer
             printf("Search: %d not found\n", val);
-            return;
+            return NULL;
         }
         if(current_node->value == val){
             printf("Search: %d found\n", val);
-            return;
+            return current_node;
         }
         else {
             current_node = current_node->next_node;
@@ -66,6 +66,11 @@ void insert(LinkedList *l, int val){
     Node *new_node = (Node*) malloc(sizeof(Node));
     if(!new_node){
         printf("Insert: error. Allocation failed\n");
+        return;
+    }
+    
+    if(search(l, val) != NULL){
+        printf("Insert: error, %d is already in the linked list\n", val);
         return;
     }
 
@@ -83,15 +88,65 @@ void insert(LinkedList *l, int val){
     }
 }
 
+void delete(LinkedList *l, int val){
+    Node *current_node = l->first;
+    
+    while (1) {
+        if(current_node == NULL || !l){
+            // Empty or invalid linked list 
+            printf("Delete: link-list is empty\n");
+            return;
+        }
+        else if(current_node->value == val){
+            // Delete link list head
+            if(current_node->next_node == NULL){
+                free(current_node);
+                l->first = NULL;
+                printf("Delete: %d is the only element. Linked list is now empty\n", val);
+            }
+            else{
+                l->first = current_node->next_node;
+                free(current_node);
+                printf("Delete: %d was the first element.\n", val);
+            }
+            return;
+        }
+        else if(current_node->next_node == NULL){
+            printf("Delete: %d not found\n", val);
+            return;
+        }
+        else if(current_node->next_node->value == val){
+            // Delete node between 2 nodes
+            Node *delete_node = current_node->next_node;
+            current_node->next_node = current_node->next_node->next_node;
+            printf("Delete: %d successfully deleted\n", val);
+            free(delete_node);
+            return;
+        }
+        else{
+            current_node = current_node->next_node;
+        }
+    }
+
+    
+    
+}
+
 
 int main(){
     LinkedList my_linked_list = init();
+    delete(&my_linked_list, 5);
     insert(&my_linked_list, 2);
+    delete(&my_linked_list, 2);
     insert(&my_linked_list, 3);
     insert(&my_linked_list, 5);
-    search(&my_linked_list, 5);
-    search(&my_linked_list, 2);
-    search(&my_linked_list, 1);
+    delete(&my_linked_list, 3);
+    insert(&my_linked_list, 2);
+    insert(&my_linked_list, 10);
+    insert(&my_linked_list, 15);
+    insert(&my_linked_list, 20);
+    delete(&my_linked_list, 15);
+    delete(&my_linked_list, 20);
 
     return 0;
 }
