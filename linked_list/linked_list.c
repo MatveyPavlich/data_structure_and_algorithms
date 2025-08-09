06995017fd84d8581032e35062a6d58f741669d9
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-09 14:02:00 +0100

    refactor: clean up linked_list.c; fix search to not return on current_node->value != val

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,3 +1,6 @@
+/* Try 1: do not store *last and length in LinkedList */
+
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -15,6 +18,7 @@ typedef struct {
 int getLength(LinkedList *l){
     int n = 0;
     Node *current_node = l->first;
+
     while (current_node != NULL){
         n++;
         current_node = current_node->next_node;
@@ -41,35 +45,39 @@ Node* getLastNode(LinkedList *l){
 }
 
 
-Node* search(LinkedList *l, int val){
+void search(LinkedList *l, int val){
     Node *current_node = l->first;
     while(1){
         if(current_node == NULL){ // Should be a first condition to not dereference a null pointer
-            return NULL;
+            printf("Search: %d not found\n", val);
+            return;
         }
         if(current_node->value == val){
-            return current_node;
+            printf("Search: %d found\n", val);
+            return;
         }
         else {
             current_node = current_node->next_node;
-            return current_node;
         }
     }
 }
 
 void insertNode(LinkedList *l, int val){
     Node *new_node = (Node*)malloc(sizeof(Node));
+    if(!new_node){
+        printf("Insert: error. Allocation failed\n");
+    }
+
     new_node->value = val;
     Node *last_node = getLastNode(l);
-
     if(last_node == NULL){
         l->first = new_node;
-        printf("insertNode: %d is the first node\n", val);
+        printf("Insert: %d is the first node\n", val);
         return;
     }
     else {
         last_node->next_node = new_node;
-        printf("insertNode: %d inserted after %d\n", val, last_node->value);
+        printf("Insert: %d inserted after %d\n", val, last_node->value);
     }
 }
 
@@ -78,6 +86,9 @@ int main(){
     LinkedList my_linked_list = init();
     insertNode(&my_linked_list, 2);
     insertNode(&my_linked_list, 3);
+    insertNode(&my_linked_list, 5);
+    search(&my_linked_list, 2);
+    search(&my_linked_list, 1);
 
     return 0;
 }
