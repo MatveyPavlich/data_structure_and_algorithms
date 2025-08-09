5cf818b37bd46bc14c4b5f76d350b5565c726b9e
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-09 21:16:10 +0100

    feat: WIP delete() for linked_list_v2.c

--- linked_list/linked_list_v2.c
+++ linked_list/linked_list_v2.c
@@ -24,7 +24,7 @@ Node* search(LinkedList *l, int val){
     
     while(current_node != NULL){
         if(current_node->value == val){
-            printf("Search: %d found\n", val);
+            printf("Search: %d found.\n", val);
             return current_node;
         }
         else{
@@ -32,19 +32,19 @@ Node* search(LinkedList *l, int val){
         }
     }
 
-    printf("Search: %d not found\n", val);
+    printf("Search: %d not found.\n", val);
     return NULL;
 }
 
 void insert(LinkedList *l, int val){
     Node *new_node = (Node*)malloc(sizeof(Node));
     if(!new_node){
-        printf("Insert: malloc failed. Opperation aborted\n");
+        printf("Insert: malloc failed. Opperation aborted.\n");
         return;
     }
 
     if(search(l, val) != NULL){
-        printf("Insert: error. %d is already in the list\n", val);
+        printf("Insert: error. %d is already in the list.\n", val);
         return;
     }
 
@@ -52,17 +52,59 @@ void insert(LinkedList *l, int val){
     new_node->next_node = NULL;
     if(l->first_node == NULL){
         l->first_node = l->last_node = new_node;
-        printf("Insert: %d is the first node\n", val);
+        printf("Insert: %d is the first node.\n", val);
     }
     else{
         Node *old_last_node = l->last_node;
         old_last_node->next_node = new_node;
         l->last_node = new_node;
-        printf("Insert: %d is added after %d\n", val, old_last_node->value);
+        printf("Insert: %d is added after %d.\n", val, old_last_node->value);
     }
 
     l->length++;
     return;
+}
+
+// WIP
+void delete(LinkedList *l, int val){
+    if(l->length == 0){
+        printf("Delete: ERROR, linked list is empty.\n");
+        return;
+    }
+    
+    if(search(l, val) == NULL){
+        printf("Delete: ERROR, %d is not in the list.\n", val);
+        return;
+    }
+    
+    Node *current_node = l->first_node;
+    if(current_node->value == val){
+        if(l->length == 1){
+            l->first_node = l->last_node = NULL;
+            printf("Delete: %d, the only element, was deleted.\n", val);
+        }
+        else{
+            l->first_node = current_node->next_node;
+            printf("Delete: first element %d.\n", val);
+        }
+        free(current_node);
+    }
+    else if(current_node->next_node->value == val){
+        Node *delete_node = current_node->next_node;
+        
+        if(current_node->next_node == l->last_node){
+            current_node->next_node = current_node->next_node->next_node;
+            l->last_node = current_node;
+            printf("Delete: last element %d deleted.\n", val);
+        }
+        else{
+            current_node->next_node = delete_node->next_node;
+        }
+    }
+
+
+
+
 }
 
 
