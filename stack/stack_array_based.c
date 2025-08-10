363f138f42a32186e3434a5fbed48447bd39cf6e
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-10 12:48:09 +0100

    feat: commit initial files for stack_array_based.c

---
+++ stack/stack_array_based.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#define MaxStack 256
+
+
+typedef struct {
+    int stack_top;
+    int stack_data[MaxStack];
+} Stack;
+
+int main(){
+    printf("All good\n");
+    return 0;
+}
+
+
