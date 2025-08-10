4527c160f4a99f41505ea8a01e70a701bbc2f97b
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-10 14:45:27 +0100

    feat: create push() for stack_array_based.c

--- stack/stack_array_based.c
+++ stack/stack_array_based.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-#define MaxStack 256
+#define MaxStack 3
 
 
 typedef struct {
-    int stack_top;
-    int stack_data[MaxStack];
+    int stackTop;
+    int stackData[MaxStack];
+    int stackSize;
 } Stack;
 
+Stack initStack(void){
+    Stack s;
+    s.stackTop = -1;
+    s.stackSize = 0;
+    return s;
+}
+
+void push(Stack *s, int val){
+    if(s->stackSize == MaxStack - 1){
+        printf("Push: stack overflow\n");
+        exit(1);
+    }
+    
+    ++(s->stackSize);
+    ++(s->stackTop);
+    s->stackData[s->stackTop] = val;
+    printf("Push: %d is on the stack.\n", val);
+    return;
+}
+
+
 int main(){
-    printf("All good\n");
+    Stack myStack = initStack();
+    push(&myStack, 1);
+    push(&myStack, 2);
+    push(&myStack, 3);
+    push(&myStack, 4);
     return 0;
 }
 
