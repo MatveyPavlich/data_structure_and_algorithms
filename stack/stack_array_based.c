af6af98cbbddef7932dcdf0a5d6e0261f57d320d
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-10 14:59:48 +0100

    feat: implement peek() for stack_array_based.c

--- stack/stack_array_based.c
+++ stack/stack_array_based.c
@@ -43,11 +43,25 @@ void pop(Stack *s) {
     return;
 }
 
+void peek(Stack *s){
+    if(s->stackSize > 0){
+        int value = s->stackData[s->stackTop];
+        printf("Peek: %d.\n", value);
+    }
+    else
+        printf("Peek: invalid operation, stack is empty.\n");
+
+    return;
+}
+
 
 int main(){
     Stack myStack = initStack();
+    peek(&myStack);
+    pop(&myStack);
     push(&myStack, 1);
     push(&myStack, 2);
+    peek(&myStack);
     push(&myStack, 3);
     pop(&myStack);
     push(&myStack, 4);
