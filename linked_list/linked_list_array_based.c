2661178433b9cfab4b14d0d764e7712f4c5272e8
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-12 07:42:16 +0100

    feat: create insert, but gives segmentation fault

--- linked_list/linked_list_array_based.c
+++ linked_list/linked_list_array_based.c
@@ -4,10 +4,37 @@
 
 
 typedef struct {
-
+	int data[MaxLinkedList];
+	int pointers[MaxLinkedList];
+ 	int firstNode;
+	int size;
 } LinkedList;
 
+LinkedList init() {
+	LinkedList l;
+	l.firstNode = -1;
+	l.size = 0;
+	return l;
+}
+
+void insert(LinkedList *l, int val) {
+	if(l->size >= MaxLinkedList) {
+		printf("Insert: error, linked list is full.\n");
+		return;
+	}
+    
+	++(l->firstNode);
+	int emptyIndex = l->data[l->firstNode];	
+	l->data[emptyIndex] = val;
+	printf("Insert: %d is written to position %d.\n", val, emptyIndex);
+	return;
+} 
+
+
 int main() {
-    printf("12\n");
-    return 0;
+	LinkedList l = init();
+	insert(&l, 2);
+	insert(&l, 3);
+	insert(&l, 4);
+	return 0;
 }
