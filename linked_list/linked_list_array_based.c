edd04af0ce9baaae2e29918da609f7e7066b89a3
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-12 18:10:26 +0100

    fix: correct insert() and init() operations

--- linked_list/linked_list_array_based.c
+++ linked_list/linked_list_array_based.c
@@ -4,37 +4,52 @@
 
 
 typedef struct {
-	int data[MaxLinkedList];
-	int pointers[MaxLinkedList];
- 	int firstNode;
-	int size;
+    int data[MaxLinkedList];     // values
+    int pointers[MaxLinkedList]; // "next" indices
+    int firstNode;               // head index, -1 if empty
+    int size;                    // number of used nodes
 } LinkedList;
 
-LinkedList init() {
-	LinkedList l;
-	l.firstNode = -1;
-	l.size = 0;
-	return l;
+LinkedList init(void) {
+    LinkedList l;
+    l.firstNode = -1;
+    l.size = 0;
+    return l;
 }
 
-void insert(LinkedList *l, int val) {
-	if(l->size >= MaxLinkedList) {
-		printf("Insert: error, linked list is full.\n");
-		return;
-	}
-    
-	++(l->firstNode);
-	int emptyIndex = l->data[l->firstNode];	
-	l->data[emptyIndex] = val;
-	printf("Insert: %d is written to position %d.\n", val, emptyIndex);
-	return;
-} 
+// Insert at head (O(1)). Uses next free index == size.
+int insert(LinkedList *l, int val) {
+    if (l->size >= MaxLinkedList) {
+        printf("Insert: error, linked list is full.\n");
+        return -1;
+    }
 
+    int idx = l->size;              // next unused slot
+    l->data[idx] = val;             // store value
+    l->pointers[idx] = l->firstNode; // link to previous head
+    l->firstNode = idx;             // new head
+    l->size++;
 
-int main() {
-	LinkedList l = init();
-	insert(&l, 2);
-	insert(&l, 3);
-	insert(&l, 4);
-	return 0;
+    printf("Insert: %d is written to node %d.\n", val, idx);
+    return idx;
+}
+
+void print_list(const LinkedList *l) {
+    printf("List (size=%d): ", l->size);
+    for (int cur = l->firstNode; cur != -1; cur = l->pointers[cur]) {
+        printf("[%d] ", l->data[cur]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    LinkedList l = init();
+
+    insert(&l, 2);
+    insert(&l, 3);
+    insert(&l, 4);
+
+    print_list(&l); // Expected order: 4 -> 3 -> 2
+
+    return 0;
 }
