b0b5c4946d6cda537f618c835cbca31459977e6c
Matvey Pavlich <matveypavlich@gmail.com>
2025-09-06 22:29:28 +0100

    feat: add search() to linked list v3

--- linked_list/linked_list_v3.c
+++ linked_list/linked_list_v3.c
@@ -36,11 +36,32 @@ void insert(LinkedList *l, int value){
     printf("Insert: %d\n", value);
 }
 
+int search(LinkedList *l, int value){
+    if(l->length == 0) {
+        printf("Search: ERROR, list is empty\n");
+        return 1;
+    }
+
+    Node *current_node = l->first_node;
+    while (current_node->val != value){
+        if(current_node->next_node == NULL) {
+            printf("Search: ERROR, %d not found\n", value);
+            return  1;
+        }
+        current_node = current_node->next_node;
+    }
+
+    printf("Search: element %d found\n", value);
+    return 0;
+}
+
 int main(){
     printf("test initial files\n");
     LinkedList l = intit_liked_list();
     insert(&l, 2);
     insert(&l, 4);
     insert(&l, 5);
+    search(&l, 4);
+    search(&l, 10);
     return 0;
 }
