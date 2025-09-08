948a1bdb476aa50565b89e3a2e8b4bdd5db18194
Matvey Pavlich <matveypavlich@gmail.com>
2025-09-08 21:50:27 +0100

    feat: implement buggy delete() into linked_list_v3

--- linked_list/linked_list_v3.c
+++ linked_list/linked_list_v3.c
@@ -68,7 +68,41 @@ void insert(LinkedList *l, int value){
 
 
 void delete(LinkedList *l, int value){
+    Node *node_to_delete = search(l, value);
 
+    if(node_to_delete == NULL) {
+        printf("Delete: ERROR, node with %d not found\n", value);
+        return;
+    }
+
+    if(l->length == 1){
+        // Deleting the only node in the list
+        free(node_to_delete);
+        --l->length;
+        l->first_node = NULL;
+        printf("Delete: %d success (only node in LL)\n", value);
+    }
+
+    Node *current_node = l->first_node;
+        
+    while(current_node->next_node == node_to_delete)
+        current_node = current_node->next_node;
+
+    if(node_to_delete->next_node == NULL){
+        // Deleting terminal node
+        
+        current_node->next_node = NULL;
+        free(node_to_delete);
+        --l->length;
+        printf("Delete: %d success (end node))\n", value);
+    }
+    else {
+        // Delete middle node
+        current_node->next_node = node_to_delete->next_node;
+        free(node_to_delete);
+        --l->length;
+        printf("Delete: %d success (middle node)\n", value);
+    }
 }
 
 
@@ -76,9 +110,16 @@ int main(){
     printf("test initial files\n");
     LinkedList l = intit_liked_list();
     insert(&l, 2);
+    delete(&l, 5);
+    insert(&l, 2);
+    insert(&l, 4);
+    delete(&l, 5);
+    delete(&l, 4);
     insert(&l, 4);
     insert(&l, 5);
     search(&l, 4);
     search(&l, 10);
+    insert(&l, 6);
+    delete(&l, 5);
     return 0;
 }
