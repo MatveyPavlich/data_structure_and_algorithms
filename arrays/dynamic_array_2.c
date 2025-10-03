d36ac6ff6854b6b5b30fee36847212951ee132ab
Matvey Pavlich <matveypavlich@gmail.com>
2025-10-03 23:03:06 +0100

    feat: create broken init_array()

--- arrays/dynamic_array_2.c
+++ arrays/dynamic_array_2.c
@@ -2,8 +2,30 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#define PAGE_SIZE 5
+
+typedef struct {
+	int page_number;
+	int *array[PAGE_SIZE];
+} DynamicArray;
+
+DynamicArray init_array(void)
+{
+	DynamicArray d;
+	d.page_number = 1;
+	d.array = malloc(sizeof(int) * 5);
+	if(d.array == NULL) {
+		printf("INIT_ARRAY:Malloc failed. Initiation aborted\n");
+		return NULL;
+	}
+	return d;
+}
+
+
 int main(void)
 {
+
+	DynamicArray my_array = init_array();
 	printf("check\n");
 	return 0;
 }
