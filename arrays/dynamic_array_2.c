207940989973a8512bd9c75f401b7d70b90624d4
Matvey Pavlich <matveypavlich@gmail.com>
2025-10-17 22:26:10 +0100

    feat: fix indentation in dynamic_array_2.c

--- arrays/dynamic_array_2.c
+++ arrays/dynamic_array_2.c
@@ -6,7 +6,6 @@
 
 #define PAGE_SIZE 2
 #define MAGIC_NUMBER -9999
-// TODO: Implement delete
 
 typedef struct {
 	int page_number;
@@ -25,32 +24,33 @@ DynamicArray init_array(void)
 		abort();
 	}
     
-    // Fill array with magic numbers that would indicate unused space
-    for(int i = 0; i < PAGE_SIZE; i++) {
-        d.array[i] = MAGIC_NUMBER;
-    }
+        // Fill array with magic numbers that would indicate unused space
+        for(int i = 0; i < PAGE_SIZE; i++) {
+                d.array[i] = MAGIC_NUMBER;
+        }
 
 	return d;
 }
 
 int search(DynamicArray* d, int value)
 {
-    // MAGIC_NUMBER is returned on no match, else index of the match
+        // MAGIC_NUMBER is returned on no match, else index of the match
 	int *array = d->array;
 	for(int i=0; i<=d->max_index; i++){
 		if(array[i] == value) return i;
 	}	
 	printf("Search: no match found for %d\n", value);
+
 	return MAGIC_NUMBER;
 }
 
 int insert(DynamicArray *d, int val)
 {
-    // Retrn: 0 on success, 1 on fail
-    if(search(d, val) != MAGIC_NUMBER) {
-        printf("Insert: ERROR, %d is already in the array\n", val);
-        return 1;
-    }
+        // Retrn: 0 on success, 1 on fail
+        if(search(d, val) != MAGIC_NUMBER) {
+                printf("Insert: ERROR, %d is already in the array\n", val);
+                return 1;
+        }
 
 	int capacity = PAGE_SIZE * d->page_number;
 	if(d->max_index + 1 == capacity){
@@ -64,6 +64,7 @@ int insert(DynamicArray *d, int val)
 		d->array = new_array;
 		printf("New page allocated. Old capacity: %d; New capacity %d\n", capacity, new_capacity);
 	}
+
 	d->array[d->max_index + 1] = val;
 	++(d->max_index);
 	printf("Insert: %d on position %d\n", val, d->max_index);
@@ -72,32 +73,32 @@ int insert(DynamicArray *d, int val)
 
 int delete(DynamicArray* d, int val)
 {
-    // TODO: handle case where the whole page was deleted
-    int del_index = search(d, val);
-    if(del_index == MAGIC_NUMBER) {
-        printf("Delete: ERROR, no such element found\n");
-        return 1;
-    }
+        // TODO: clean up the code!
+        int del_index = search(d, val);
+        if(del_index == MAGIC_NUMBER) {
+                printf("Delete: ERROR, no such element found\n");
+                return 1;
+        }
+ 
+        int *array = d->array;
+         
+        // Case where the whole page should be deleted
+        if(del_index == (d->page_number - 1) * PAGE_SIZE && d->page_number > 1) {
+                --(d->page_number);
+                int new_capacity = (d->page_number) * PAGE_SIZE;
+                d->array = realloc(array, sizeof(int) * new_capacity);
+                printf("Delete: %d was the last element on the page. Page %d is removed.\n", val, d->page_number);
+                return 0;
+        }
+
+        array[del_index] = 0;
+        for(int i = del_index; i < d->max_index; i++)
+                array[i] = array[i+1];
     
-    int *array = d->array;
-    
-    // Case where the whole page should be deleted
-    if(del_index == (d->page_number - 1) * PAGE_SIZE && d->page_number > 1) {
-        --(d->page_number);
-        int new_capacity = (d->page_number) * PAGE_SIZE;
-        d->array = realloc(array, sizeof(int) * new_capacity);
-        printf("Delete: %d was the last element on the page. Page %d is removed.\n", val, d->page_number);
+        --(d->max_index);
+        printf("Delete: SUCCESS, %d was removed\n", val);
+
         return 0;
-    }
-
-    array[del_index] = 0;
-    for(int i = del_index; i < d->max_index; i++)
-        array[i] = array[i+1];
-    
-    --(d->max_index);
-    printf("Delete: SUCCESS, %d was removed\n", val);
-
-    return 0;
 }
 
 int main(void)
@@ -108,10 +109,10 @@ int main(void)
 	insert(&my_array, 3);
 	insert(&my_array, 4);
 	search(&my_array,4);
-    delete(&my_array, 100);
-    delete(&my_array, 4);
-    insert(&my_array, 3);
-    delete(&my_array, 3);
-    return 0;
+        delete(&my_array, 100);
+        delete(&my_array, 4);
+        insert(&my_array, 3);
+        delete(&my_array, 3);
+        return 0;
 }
 
