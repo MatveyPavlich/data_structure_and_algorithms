c6971d52a4d282625c72603aa5b21a9e43c030fc
Matvey Pavlich <matveypavlich@gmail.com>
2025-10-25 21:31:20 +0100

    feat: create necessary structs for a hashmap

--- hash_table/hash_table_1.c
+++ hash_table/hash_table_1.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define TABLE_SIZE 100
+
+typedef struct hash_node {
+        char* string;
+        int value;
+} HashNode;
+
+typedef struct {
+        HashNode **buckets;
+} HashMap;
 
 int main(void)
 {
