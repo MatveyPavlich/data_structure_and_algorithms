74afdd9313c00bb67475d6505536aa6623e0fb6e
Matvey Pavlich <matveypavlich@gmail.com>
2025-10-27 22:18:49 +0000

    feat: solve 231A codeforces

---
+++ codeforces/231A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int n;
+        scanf("%d", &n);
+
+        int problems_solved = 0;
+
+        for(int i=0; i<n; i++){
+                int p, v, t;
+                scanf("%d %d %d", &p, &v, &t);
+                
+                if(p+v+t > 1)
+                        ++problems_solved;
+        }
+
+        printf("%d\n", problems_solved);
+        return 0;
+}
