f3395b7f23aaf60a803fed3ad5b3d45f064376a0
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-13 22:31:10 +0000

    feat: correct solution to 486A.c

--- codeforces/486A.c
+++ codeforces/486A.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-// If n = 10^15 then thats a lot of itterations...
 int main(void) {
         long long n;
         scanf("%lld", &n);
-        long long sum = 0;
-        int multiplier = 1;
-        for (long long i = 1; i <= n; i++) {
-                sum += multiplier * i;
-                multiplier *= -1;
-        }
-        printf("%lld\n", sum);
+
+        if (n % 2 == 0)
+                printf("%lld\n", n / 2);
+        else
+                printf("%lld\n", -((n + 1) / 2));
+
         return 0;
 }
+
+// // If n = 10^15 then thats a lot of itterations...
+// int main(void) {
+//         long long n;
+//         scanf("%lld", &n);
+//         long long sum = 0;
+//         int multiplier = 1;
+//         for (long long i = 1; i <= n; i++) {
+//                 sum += multiplier * i;
+//                 multiplier *= -1;
+//         }
+//         printf("%lld\n", sum);
+//         return 0;
+// }
