4701f3ed002fe9419ec63664343f13c919e00a1e
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-16 23:01:44 +0000

    feat: 705A.c codeforces

---
+++ codeforces/705A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+        int n;
+        scanf("%d", &n);
+
+        for (int i = 1; i <= n; i++) {
+                if (i % 2 == 1) printf("I hate");
+                else printf("I love");
+
+                if (i == n) printf(" it");
+                else printf(" that ");
+        }
+        printf("\n");
+
+        return 0;
+}
