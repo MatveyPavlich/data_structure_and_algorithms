8f5bff1915725b5114d8b88000aa969870e3c8ed
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-18 22:35:11 +0000

    buggy solution to 520A, need help from gpt...

--- codeforces/520A.c
+++ codeforces/520A.c
@@ -7,9 +7,23 @@ int main(void) {
         char s[n];
         scanf("%s", s);
 
-        for (int i = 0; i < n; i++)
-                s[i] = tolower(s[i]);
-        printf("%s\n", s);
+        char alphabet[25];
+
+        for (int i = 0; i < n; i++) {
+                s[i] = toupper(s[i]);
+                printf("%d\n", (int)(s[i] - 65));
+                fflush(stdout);
+                alphabet[(s[i] - 65)] = 1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 25; i++)
+                count += alphabet[i];
+
+        if (count == 25)
+                printf("YES\n");
+        else
+                printf("NO\n");
 
         return 0;
 }
