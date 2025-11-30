97de12eb060cf0bded8756292d05aa43ed78a7b0
Matvey Pavlich <matveypavlich@gmail.com>
2025-11-30 11:33:26 +0000

    refactor: 2nd way to slove 1669A.c

--- codeforces/1669A.c
+++ codeforces/1669A.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
+// Way 2: more memory efficient solution
 int main(void) {
         int t;
         scanf("%d", &t);
-        char arr[t][12];
+        int arr[t];
 
         int tmp = 0;
         for (int i = 0; i < t; i++) {
                 scanf("%d", &tmp);
 
                 if (tmp >= 1900)
-                        strcpy(arr[i], "Division 1\n");
+                        arr[i] = 1;
                 else if (tmp >= 1600)
-                        strcpy(arr[i], "Division 2\n");
+                        arr[i] = 2;
                 else if (tmp >= 1400)
-                        strcpy(arr[i], "Division 3\n");
+                        arr[i] = 3;
                 else
-                        strcpy(arr[i], "Division 4\n");
+                        arr[i] = 4;
         }
 
         for (int j = 0; j < t; j++)
-                printf("%s", arr[j]);
+                printf("Division %d\n", arr[j]);
 
         return 0;
 }
+// // Way 1: flexing you string skills in C
+// int main(void) {
+//         int t;
+//         scanf("%d", &t);
+//         char arr[t][12];
+//
+//         int tmp = 0;
+//         for (int i = 0; i < t; i++) {
+//                 scanf("%d", &tmp);
+//
+//                 if (tmp >= 1900)
+//                         strcpy(arr[i], "Division 1\n");
+//                 else if (tmp >= 1600)
+//                         strcpy(arr[i], "Division 2\n");
+//                 else if (tmp >= 1400)
+//                         strcpy(arr[i], "Division 3\n");
+//                 else
+//                         strcpy(arr[i], "Division 4\n");
+//         }
+//
+//         for (int j = 0; j < t; j++)
+//                 printf("%s", arr[j]);
+//
+//         return 0;
+// }
