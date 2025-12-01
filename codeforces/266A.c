b5d49377b72994706daa290d6629361521598106
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-01 19:27:03 +0000

    feat: 266A.c revisited

--- codeforces/266A.c
+++ codeforces/266A.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
+        int n;
+        scanf("%d", &n);
 
-	int n;
-	scanf("%d", &n);
-	char s[51];
-	scanf("%s", s);
-	return 0;
+        char s[51];
+        scanf("%s", s);
+
+        int remove = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+                if (s[i] == s[i + 1])
+                        remove++; 
+        }
+
+        printf("%d\n", remove);
+
+        return 0;
 }
