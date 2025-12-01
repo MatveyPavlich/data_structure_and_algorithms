0856fff7ee5d8f35999f45fa9bb7224c2dd0f596
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-01 19:41:37 +0000

    feat: 546A.c codeforces finished

--- codeforces/546A.c
+++ codeforces/546A.c
@@ -5,8 +5,11 @@ int main(void)
 	int k, n, w;
 	scanf("%d %d %d", &k, &n, &w);
 	
-	int ttl = k * (w * 2) / 2;
-
+	int ttl = k * w * (w + 1) / 2;
+        int borrow = ttl - n;
+        if (borrow < 0)
+                borrow = 0;
+        printf("%d\n", borrow);
 
 	return 0;
 }
