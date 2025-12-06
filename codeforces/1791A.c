2329bb13fc89ed2255c12df8b3ec5f1f947a0c03
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-06 12:21:34 +0000

    refactor:1791A.c done in a more efficient way

--- codeforces/1791A.c
+++ codeforces/1791A.c
@@ -7,27 +7,38 @@ int main(void) {
         char arr[t][5];
 
         char c;
+        char *s = "codeforces";
         for (int i = 0; i < t; i++) {
                 scanf(" %c", &c);
-                if (c == 'c' ||
-                                c == 'o' ||
-                                c == 'd' ||
-                                c == 'e' ||
-                                c == 'f' ||
-                                c == 'o' ||
-                                c == 'r' ||
-                                c == 's' )
+                if (strchr(s, c))
                         strcpy(arr[i], "YES\n");
                 else
                         strcpy(arr[i], "NO\n");
-
-                // switch(c) {
-                //         case 'a': printf("Char a\n"); break;
-                //         case 'b': printf("Char b\n"); break;
-                // }
         }
+
 
         for (int i = 0; i < t; i++)
                 printf("%s", arr[i]);
         return 0;
 }
+
+// // Way 1: hardcode a bunch of OR statements
+// for (int i = 0; i < t; i++) {
+//         scanf(" %c", &c);
+//         if (c == 'c' ||
+//                         c == 'o' ||
+//                         c == 'd' ||
+//                         c == 'e' ||
+//                         c == 'f' ||
+//                         c == 'o' ||
+//                         c == 'r' ||
+//                         c == 's' )
+//                 strcpy(arr[i], "YES\n");
+//         else
+//                 strcpy(arr[i], "NO\n");
+// }
+
+// switch(c) {
+//         case 'a': printf("Char a\n"); break;
+//         case 'b': printf("Char b\n"); break;
+// }
