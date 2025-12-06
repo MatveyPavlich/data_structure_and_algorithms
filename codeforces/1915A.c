c68e10c169456f22117c7464df3ea78738bbf631
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-06 13:04:12 +0000

    refactor:1915A.c with a more efficient way

--- codeforces/1915A.c
+++ codeforces/1915A.c
@@ -7,12 +7,20 @@ int main(void) {
         int a, b, c;
         for (int i = 0; i < t; i++) {
                 scanf("%d %d %d", &a, &b, &c);
-                if (a == b)
-                        arr[i] = c;
-                else if (a == c)
-                        arr[i] = b;
-                else
-                        arr[i] = a;
+                arr[i] = a ^ b ^ c; 
+
+                /* ======================================
+                * Way 1: My initial method
+                * if      (a == b) arr[i] = c;
+                * else if (a == c) arr[i] = b;
+                * else             arr[i] = a;
+                *   
+                * // Way 2: ternary logic
+                * arr[i] = (a == b ? c : (a == c ? b : a));
+                * 
+                * // Way 3; using bitwise XOR (coldest...)
+                * arr[i] = a ^ b ^ c; 
+                * ======================================*/
         }
 
         for (int i = 0; i < t; i++)
