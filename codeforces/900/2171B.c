4b317162c20a24d1d6f572c91f646e0fa9a9bed1
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-15 19:48:29 +0000

    feat: 2171B.c done with the help of GPT, but not 100% sure I understand the solution

--- codeforces/900/2171B.c
+++ codeforces/900/2171B.c
@@ -1,28 +1,59 @@
 #include <stdio.h>
 #define MAXN 200000
 
-int a[MAXN]; // To store raw arrays
-
+int a[MAXN];
 
 int main(void) {
         int t;
         scanf("%d", &t);
+
         int start[t];
         int len[t];
         int pos = 0;
+
+        // Input 
         for (int i = 0; i < t; i++) {
                 scanf("%d", &len[i]);
                 start[i] = pos;
-                for (int j = 0; j < len[i]; j++)
+                for (int j = 0; j < len[i]; j++) {
                         scanf("%d", &a[pos++]);
+                }
         }
 
+        // Each test case 
         for (int i = 0; i < t; i++) {
-                printf("%d\n", len[i]);
-                for (int j = 0; j < len[i]; j++)
-                        printf("%d ", a[start[i] + j]);
+                int n = len[i];
+                int *arr = &a[start[i]];
+
+                int left = arr[0];
+                int right = arr[n - 1];
+
+                // Decide optimal ends
+                if (left == -1 && right == -1) {
+                        arr[0] = 0;
+                        arr[n - 1] = 0;
+                } else if (left == -1) {
+                        arr[0] = right;
+                } else if (right == -1) {
+                        arr[n - 1] = left;
+                }
+
+                // Fill middle blanks with 0
+                for (int j = 1; j < n - 1; j++) {
+                        if (arr[j] == -1)
+                                arr[j] = 0;
+                }
+
+                // Compute minimum value |a[n-1] - a[0]|
+                int score = arr[n - 1] - arr[0];
+                if (score < 0) score = -score;
+
+                // Output 
+                printf("%d\n", score);
+                for (int j = 0; j < n; j++)
+                        printf("%d ", arr[j]);
                 printf("\n");
         }
-                                
+
         return 0;
 }
