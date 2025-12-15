342e77f5dc94428a45773b87bd2e8d3a88ceab8f
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-15 19:33:39 +0000

    feat: 2171B.c output working

--- codeforces/900/2171B.c
+++ codeforces/900/2171B.c
@@ -3,23 +3,26 @@
 
 int a[MAXN]; // To store raw arrays
 
+
 int main(void) {
         int t;
-        // int output[t];   // store min value of the sum for each case
         scanf("%d", &t);
-        int n[t];        // Store n for each test case 
+        int start[t];
+        int len[t];
+        int pos = 0;
         for (int i = 0; i < t; i++) {
-                scanf("%d", &n[i]);
-                for (int j = 0; j < n[i]; j++) {
-                        scanf("%d", &a[j]);
-                }
+                scanf("%d", &len[i]);
+                start[i] = pos;
+                for (int j = 0; j < len[i]; j++)
+                        scanf("%d", &a[pos++]);
         }
 
         for (int i = 0; i < t; i++) {
-                printf("%d\n", n[i]);
-                for (int j = 0; j < n[i]; j++) printf("%d" , a[j]);
+                printf("%d\n", len[i]);
+                for (int j = 0; j < len[i]; j++)
+                        printf("%d ", a[start[i] + j]);
                 printf("\n");
-        }                        
+        }
                                 
         return 0;
 }
