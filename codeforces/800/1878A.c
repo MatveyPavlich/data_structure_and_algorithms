7ab69e7c7ea07d7f44cf17de2e0b3fcd5682824c
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-20 23:06:48 +0000

    feat: 1878A.c done. Didn't know what a subsegment was...

--- codeforces/800/1878A.c
+++ codeforces/800/1878A.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
         int t;
         scanf("%d", &t);
-        int n, k;
+
         while (t--) {
+                int n, k;
                 scanf("%d %d", &n, &k);
-                int arr[n];
-                for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
-                int freq[101] = {0};
-                for (int i = 0; i < n; i++) freq[arr[i]]++;
-                int max_freq = 0;
-                int k_freq = 0;
-                for (int i = 0; i < 101; i++) {
-                        if (freq[i] > max_freq) max_freq = freq[i];
-                        if (i == k)
-                                k_freq = freq[i];
+
+                int frequency = 0;
+                for (int i = 0; i < n; i++) {
+                        int x;
+                        scanf("%d", &x);
+                        if (x == k) frequency = 1;
                 }
 
-                printf("%s\n", k_freq >= max_freq ? "YES" : "NO");
-
+                printf(frequency ? "YES\n" : "NO\n");
         }
+
         return 0;
 }
