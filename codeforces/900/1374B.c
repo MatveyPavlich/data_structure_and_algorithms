33a91c0502c2c50b12128a3fbeee45390ebc4843
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-23 10:10:43 +0000

    feat: 1374B.c codeforces

---
+++ codeforces/900/1374B.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int t;
+        scanf("%d", &t);
+        int n;
+        while (t--) {
+                scanf("%d", &n);
+                // Either *2 or / 2 /3
+                // A number needs
+                //
+                int twos = 0;
+                int threes = 0;
+                int temp = n;
+                while (temp % 2 == 0 || temp % 3 == 0) {
+                        if (temp % 2 == 0) {
+                                twos++;
+                                temp /= 2;
+                        }
+                        if (temp % 3 == 0) {
+                                threes++;
+                                temp /= 3;
+                        }
+                }
+                // printf("Num %d (remainder = %d): 2^(%d), 3^(%d)\n", n, temp, twos, threes);
+
+                if (n == 1)                         printf("%d\n", 0);
+                else if ((twos == 0 && threes == 0)
+                         || threes < twos
+                         || temp > 1)               printf("%d\n", -1);
+                else {
+                        printf("%d\n", (threes - twos) + threes);
+                }
+        }
+        return 0;
+}
