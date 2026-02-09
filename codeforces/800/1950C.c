91324325c86a61c778d0a50c35646cf7823f05a2
Matvey Pavlich <matveypavlich@gmail.com>
2026-02-09 20:13:29 +0000

    feat: 1950C.c codeforeces. The code looks absolute garbage

---
+++ codeforces/800/1950C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int t;
+        scanf("%d", &t);
+        char s[6];
+        while (t--) {
+                scanf("%s", s);
+                int hours = (s[0] - '0') * 10 + (s[1] - '0');
+                // int hours = atoi(&s[0]) * 10 + atoi(&s[1]);
+                if (hours == 0)
+                        printf("12:%c%c AM\n", s[3], s[4]);
+                else if (hours < 12)
+                        printf("%s AM\n", s);
+                else if (hours == 12)
+                        printf("12:%c%c PM\n", s[3], s[4]);
+                else if (hours < 22)
+                        printf("0%d:%c%c PM\n", (hours - 12), s[3], s[4]);
+                else
+                        printf("%d:%c%c PM\n", (hours - 12), s[3], s[4]);
+        }
+        return 0;
+}
