b059ea91930de1ff1ff5e66c127f2e1d2564476a
Matvey Pavlich <matveypavlich@gmail.com>
2026-03-06 22:43:24 +0000

    feat: 1337A.c codeforces

---
+++ codeforces/800/1337A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int t;
+        scanf("%d", &t);
+
+        while (t--) {
+                long long a, b, c, d;
+                scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+                printf("%lld %lld %lld\n", b, c, c);
+        }
+
+        return 0;
+}
