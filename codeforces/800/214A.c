7c581873a66d92ae08ae50e3759a0ae863a696ba
Matvey Pavlich <matveypavlich@gmail.com>
2026-03-09 21:43:40 +0000

    feat: 214A.c codeforces

---
+++ codeforces/800/214A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int n, m;
+        scanf("%d %d", &n, &m);
+
+        int count = 0;
+
+        for (int a = 0; a * a <= n; a++) {
+                int b = n - a * a;
+
+                if (a + b * b == m)
+                        count++;
+        }
+
+        printf("%d\n", count);
+
+        return 0;
+}
